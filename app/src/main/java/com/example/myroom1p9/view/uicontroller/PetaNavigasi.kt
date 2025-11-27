7cbcc119ff0212319622d32ecc0466a1e77874da
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-27 14:28:28 +0700

    Konfigurasi HostNavigasi dengan rute DestinasiHome

--- app/src/main/java/com/example/myroom1p9/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myroom1p9/view/uicontroller/PetaNavigasi.kt
@@ -4,7 +4,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.myroom1p9.view.HomeScreen
+import com.example.myroom1p9.view.route.DestinasiEntry
+import com.example.myroom1p9.view.route.DestinasiHome
 
 @Composable
 fun SiswaApp(
@@ -20,5 +25,15 @@ fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(route = DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
 
+    }
 }
