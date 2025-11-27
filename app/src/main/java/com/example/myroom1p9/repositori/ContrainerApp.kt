7621774bbebb57ab7b5fe59874e9efa757270d7c
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-27 09:40:28 +0700

    Tambah class AplikasiSiswa untuk inisialisasi dependency

--- app/src/main/java/com/example/myroom1p9/repositori/ContrainerApp.kt
+++ app/src/main/java/com/example/myroom1p9/repositori/ContrainerApp.kt
@@ -1,5 +1,6 @@
 package com.example.myroom1p9.repositori
 
+import android.app.Application
 import android.content.Context
 import com.example.myroom1p9.room.DatabaseSiswa
 
@@ -17,3 +18,15 @@ class ContainerDataApp(private val context: Context) :
     }
 }
 
+class AplikasiSiswa : Application() {
+
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
